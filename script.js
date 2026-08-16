d47553c3089efc0a52fe8161240f861e419d08e7
unknown <nehashakya18609@gmail.com>
2026-08-16 20:09:02 +0530

    Connect frontend to production API

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_URL = "http://127.0.0.1:8000";
+const API_URL = "https://task-flow-rtky.onrender.com";
 const TASKS_CACHE_KEY = "tasks";
 
 const taskForm = document.getElementById("task-form");
